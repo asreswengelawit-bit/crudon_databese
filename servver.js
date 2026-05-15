03b24e5248f16229c28fa8fbdeeafd78d6cdb0bf
asreswengelawit-bit <asreswengelawit@gmail.com>
2026-05-14 23:53:46 -0800

    Add files via upload

--- servver.js
+++ servver.js
@@ -1,5 +1,36 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const app = express();
 app.listen (3000,() =>{
     console.log("server is runing on port 3000");
+});
+app.get ('/',(req,res)=>{
+    res.send("hello from node api")
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/testdb')
+.then(() => console.log('Connected'))
+.catch(err => console.log(err));
+const ProductSchema = new mongoose.Schema({
+    name: String,
+    price: Number
+});
+
+const Product = mongoose.model('Product', ProductSchema);
+app.get('/products', async (req, res) => {
+
+    try {
+
+        const products = await Product.find();
+
+        res.json(products);
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+
 });
